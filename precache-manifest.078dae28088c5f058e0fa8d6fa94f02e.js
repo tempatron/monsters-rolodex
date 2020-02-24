4235877692441118909d05ecf93ce1f579a8366a
Abhinav <abhinavraut420@gmail.com>
2020-02-25 04:23:05 +0530

    Updates

---
+++ precache-manifest.078dae28088c5f058e0fa8d6fa94f02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290b19acaf4366e08d779651d7972495",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bc12c4f995bfc21e31aa",
+    "url": "/monsters-rolodex/static/css/main.5713a302.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc12c4f995bfc21e31aa",
+    "url": "/monsters-rolodex/static/js/main.3a739bd1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
