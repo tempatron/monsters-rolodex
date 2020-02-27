ca5fc288587c33c685d03a74c8b6890a7d5ce651
Abhinav <abhinavraut420@gmail.com>
2020-02-27 10:55:36 +0530

    initial commit

---
+++ build/precache-manifest.0dc1d47c725d7290bb9d80f8709976b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f59de79b3d186be6b63fcb2fed773",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fd9f86fb0654732de70",
+    "url": "/monsters-rolodex/static/css/main.5713a302.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd9f86fb0654732de70",
+    "url": "/monsters-rolodex/static/js/main.b4bc0645.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
